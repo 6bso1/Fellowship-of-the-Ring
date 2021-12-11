bac279b6c30a27b740fe2dd24e8ccaa8a7ba1e7c
yalcinalaman <yalcin.alaman@gmail.com>
2021-12-11 20:24:08 +0300

    Background Builder has been added
    You can use this function to draw background. You should use this in stack

---
+++ sportal/lib/screens/Background.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.cover)),
+    );
+  }
+}

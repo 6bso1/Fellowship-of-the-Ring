b58afc488e535207891cdccf7e3cc4b43468c771
yalcinalaman <yalcin.alaman@gmail.com>
2021-12-11 20:25:24 +0300

    SahaBul Page has been added.
    There are only app bar, background and navigation bar. I will add the rest.

---
+++ sportal/lib/screens/SahaBul_s.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Background.dart';
+import 'HomePage_s.dart';
+
+class RouteSahaBul extends StatelessWidget {
+  const RouteSahaBul({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: buildBottomAppBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: buildFloatingActionButton(context),
+      appBar: buildHeader(),
+      extendBodyBehindAppBar:
+          true, //Body'i appbar kısmına çekiyor (Arka planı uzatmak için)
+      body: Stack(children: <Widget>[
+        //Arkaplanın üzerine eklenecek widgetlar için stack kullandım
+        Background(),
+        SafeArea(child: SingleChildScrollView(child: Column()))
+      ]),
+    );
+  }
+}
+
+AppBar buildHeader() {
+  return AppBar(
+      elevation: 0.0,
+      title: Center(
+          child: Image.asset('assets/images/Header-Saha-Bul.png',
+              height: AppBar()
+                  .preferredSize
+                  .height)), //image'i app bar'ın yüksekliğine görse resize ediyor
+      backgroundColor: Colors.transparent, //AppBar'ı tramsparan yapıyor
+      automaticallyImplyLeading: false);
+}

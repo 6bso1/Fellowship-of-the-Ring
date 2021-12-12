02ce5d548e87b9f9a62ffd46dbde44ce3e673ad3
Burcu Sultan Orhan <burccuso@gmail.com>
2021-12-12 17:01:42 +0300

    filenames

--- sportal/lib/screens/ResetPassword_s.dart
+++ sportal/lib/screens/ResetPassword_s.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:sportal/screens/signup_s.dart';
 
 import 'Login_s.dart';
 

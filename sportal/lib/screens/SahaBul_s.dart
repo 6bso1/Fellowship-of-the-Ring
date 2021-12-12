ba21449288e4000461922147d732d1aa161bcb15
yalcinalaman <yalcin.alaman@gmail.com>
2021-12-13 01:23:33 +0300

    dummy sahabul with astroturf list added

--- sportal/lib/screens/SahaBul_s.dart
+++ sportal/lib/screens/SahaBul_s.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,10 +6,12 @@ import 'Background.dart';
 import 'HomePage_s.dart';
 
 class RouteSahaBul extends StatelessWidget {
-  const RouteSahaBul({Key? key}) : super(key: key);
+  RouteSahaBul({Key? key}) : super(key: key);
+  final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
+    CollectionReference sahaRef = firestore.collection('user');
     return Scaffold(
       bottomNavigationBar: buildBottomAppBar(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -19,7 +22,45 @@ class RouteSahaBul extends StatelessWidget {
       body: Stack(children: <Widget>[
         //Arkaplanın üzerine eklenecek widgetlar için stack kullandım
         Background(),
-        SafeArea(child: SingleChildScrollView(child: Column()))
+        SafeArea(
+            child: Column(
+          children: [
+            StreamBuilder<QuerySnapshot>(
+              stream: sahaRef.snapshots(),
+              builder: (BuildContext context, AsyncSnapshot asyncSnapshot) {
+                if (asyncSnapshot.hasError) {
+                  return Center(child: Text("Bir Hata oluştu."));
+                } else {
+                  if (asyncSnapshot.hasData) {
+                    List<DocumentSnapshot> listOfFields =
+                        asyncSnapshot.data.docs;
+                    return Flexible(
+                      child: ListView.builder(
+                          itemCount: listOfFields.length,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              color: Colors.red,
+                              child: ListTile(
+                                title: Text(
+                                    '${listOfFields[index].get('name')}',
+                                    style: TextStyle(
+                                        fontSize: 24, color: Colors.white)),
+                                subtitle: Text(
+                                    '${listOfFields[index].get('adress')}',
+                                    style: TextStyle(
+                                        fontSize: 12, color: Colors.white)),
+                              ),
+                            );
+                          }),
+                    );
+                  } else {
+                    return Center(child: CircularProgressIndicator());
+                  }
+                }
+              },
+            )
+          ],
+        ))
       ]),
     );
   }

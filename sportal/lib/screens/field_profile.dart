955d093b4d2aea459ec82a105b158334af065467
yalcinalaman <yalcin.alaman@gmail.com>
2022-01-15 15:41:52 +0300

    Scrolling has been added to the field pages

--- sportal/lib/screens/field_profile.dart
+++ sportal/lib/screens/field_profile.dart
@@ -42,166 +42,155 @@ class FieldProfileState extends State<FieldProfile> {
         //Arkaplanın üzerine eklenecek widgetlar için stack kullandım
         Background(),
         SafeArea(
-            child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-                height: 250,
-                width: double.infinity,
-                child: PageView.builder(
-                    itemCount: images.length,
-                    pageSnapping: true,
-                    controller: _pageController,
-                    onPageChanged: (page) {
-                      setState(() {
-                        activePage = page;
-                      });
-                    },
-                    itemBuilder: (context, pagePosition) {
-                      return Container(
-                        child: Image.network(images[pagePosition]),
-                      );
-                    })),
-            Row(
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  height: 250,
+                  width: double.infinity,
+                  child: PageView.builder(
+                      itemCount: images.length,
+                      pageSnapping: true,
+                      controller: _pageController,
+                      onPageChanged: (page) {
+                        setState(() {
+                          activePage = page;
+                        });
+                      },
+                      itemBuilder: (context, pagePosition) {
+                        return Container(
+                          child: Image.network(images[pagePosition]),
+                        );
+                      })),
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: indicators(images.length, activePage)),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                fieldVar.get('name'),
+                style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: indicators(images.length, activePage)),
-            SizedBox(
-              height: 15,
-            ),
-            Text(
-              fieldVar.get('name'),
-              style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  fieldVar.get('commenNum').toString(),
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-                Text(
-                  ' Değerlendirme   ',
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-                Text(
-                  fieldVar.get('rate').toString(),
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-                SizedBox(width: 2),
-                Image.asset(
-                  'assets/images/star.png',
-                  height: 16,
-                ),
-                SizedBox(width: 4),
-                Text(
-                  fieldVar.get('favNum').toString(),
-                  style: TextStyle(fontSize: 14, color: Colors.white),
-                ),
-                SizedBox(width: 2),
-                Image.asset(
-                  'assets/images/fav.png',
-                  height: 16,
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            Text(
-              fieldVar.get('properties'),
-              style: TextStyle(fontSize: 14, color: Colors.white),
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
-              child: Column(children: [
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/location.png',
-                      height: 25,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      fieldVar.get('adress').toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/price.png',
-                      height: 25,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      fieldVar.get('cost').toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/clock.png',
-                      height: 25,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      fieldVar.get('adress').toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/phone.png',
-                      height: 25,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      fieldVar.get('phone').toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/phone.png',
-                      height: 25,
-                    ),
-                    SizedBox(width: 4),
-                    Text(
-                      fieldVar.get('phone').toString(),
-                      style: TextStyle(fontSize: 14, color: Colors.white),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          ],
+                children: [
+                  Text(
+                    fieldVar.get('commenNum').toString(),
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                  Text(
+                    ' Değerlendirme   ',
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                  Text(
+                    fieldVar.get('rate').toString(),
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                  SizedBox(width: 2),
+                  Image.asset(
+                    'assets/images/star.png',
+                    height: 16,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    fieldVar.get('favNum').toString(),
+                    style: TextStyle(fontSize: 14, color: Colors.white),
+                  ),
+                  SizedBox(width: 2),
+                  Image.asset(
+                    'assets/images/fav.png',
+                    height: 16,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 12,
+              ),
+              Text(
+                fieldVar.get('properties'),
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                child: Column(children: [
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/location.png',
+                        height: 25,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        fieldVar.get('adress').toString(),
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/price.png',
+                        height: 25,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        fieldVar.get('cost').toString(),
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/clock.png',
+                        height: 25,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        fieldVar.get('adress').toString(),
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Image.asset(
+                        'assets/images/phone.png',
+                        height: 25,
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        fieldVar.get('phone').toString(),
+                        style: TextStyle(fontSize: 14, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                ]),
+              ),
+            ],
+          ),
         ))
       ]),
     );

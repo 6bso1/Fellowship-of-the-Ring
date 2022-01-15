09478cf5a654b1bc9fdd0234d481877a86b54fa3
yalcinalaman <yalcin.alaman@gmail.com>
2022-01-15 16:24:46 +0300

    buttons are centered

--- sportal/lib/bars/bottom_bar_floating_action_button.dart
+++ sportal/lib/bars/bottom_bar_floating_action_button.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+
 import '../screens/HomePage_s.dart';
 
-Widget buildFloating(BuildContext context){
+Widget buildFloating(BuildContext context) {
   return FloatingActionButton(
     onPressed: () {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) =>
-                  MyApp()));
+      Navigator.push(context, MaterialPageRoute(builder: (context) => MyApp()));
     },
     child: CircleAvatar(
       radius: 30,
       backgroundColor: Colors.purple,
       child: CircleAvatar(
-        //child: Image.asset("assets/swirl_and_ball.png"),
-        backgroundImage: NetworkImage("https://github.com/okantorun/Vector-Graphics-Libraray-C/blob/main/JPGs/swirl_and_ball.png?raw=true"),
-        radius:20,
+        backgroundImage: AssetImage('assets/images/swirl_and_ball.png'),
+        radius: 20,
       ),
     ),
   );
--- sportal/lib/bars/bottom_bar_player_search.dart
+++ sportal/lib/bars/bottom_bar_player_search.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sportal/screens/messageSearch_s.dart';
-import '../screens/player_profile.dart';
-import '../screens/my_profile.dart';
+
 import '../screens/messageSearch_s.dart';
+import '../screens/my_profile.dart';
 
 Widget buildBottomBar() {
   return _DemoBottomAppBar(
@@ -33,49 +33,55 @@ class _DemoBottomAppBar extends StatelessWidget {
       color: Color(0xFF4AC5F6),
       child: IconTheme(
         data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
-        child: Row(
-          children: <Widget>[
-            IconButton(
-              tooltip: 'Favorite',
-              icon: const Icon(Icons.favorite),
-              iconSize: 35,
-              color: Colors.black,
-              onPressed: () {},
-            ),
-            IconButton(
-              tooltip: 'Search',
-              icon: const Icon(Icons.search),
-              iconSize: 35,
-              color: Colors.black,
-              onPressed: () {
-
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MessageSearchScreen()));
-              },
-            ),
-            if (centerLocations.contains(fabLocation)) const Spacer(),
-            IconButton(
-              tooltip: 'Message',
-              icon: const Icon(Icons.message),
-              iconSize: 35,
-              color: Colors.black,
-              onPressed: () {
-
-              },
-            ),
-            IconButton(
-              tooltip: 'Profile',
-              icon: const Icon(Icons.account_circle),
-              iconSize: 35,
-              color: Colors.black,
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => MyProfileUI(0)));
-              },
-            ),
-          ],
+        child: Container(
+          margin: EdgeInsets.fromLTRB(15, 0, 15, 0),
+          child: Row(
+            children: <Widget>[
+              IconButton(
+                tooltip: 'Favorite',
+                icon: const Icon(Icons.favorite),
+                iconSize: 35,
+                color: Colors.black,
+                onPressed: () {},
+              ),
+              Expanded(
+                flex: 1,
+                child: IconButton(
+                  tooltip: 'Search',
+                  icon: const Icon(Icons.search),
+                  iconSize: 35,
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => MessageSearchScreen()));
+                  },
+                ),
+              ),
+              if (centerLocations.contains(fabLocation)) const Spacer(),
+              Expanded(
+                flex: 1,
+                child: IconButton(
+                  tooltip: 'Message',
+                  icon: const Icon(Icons.message),
+                  iconSize: 35,
+                  color: Colors.black,
+                  onPressed: () {},
+                ),
+              ),
+              IconButton(
+                tooltip: 'Profile',
+                icon: const Icon(Icons.account_circle),
+                iconSize: 35,
+                color: Colors.black,
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyProfileUI(0)));
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );

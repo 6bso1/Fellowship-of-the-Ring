a2ba054298867794b794eca518f3d6242237748f
yalcinalaman <yalcin.alaman@gmail.com>
2022-01-21 06:46:48 +0300

    x

--- sportal/lib/screens/SahaBul_s.dart
+++ sportal/lib/screens/SahaBul_s.dart
@@ -2,11 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:sportal/model/Drowdownlist.dart';
 
 import '../bars/bottom_bar_floating_action_button.dart';
 import '../bars/bottom_bar_player_search.dart';
 import 'Background.dart';
 import 'field_profile.dart';
+import 'il-secim.dart';
+import 'ilce-secim.dart';
 
 class RouteSahaBul extends StatefulWidget {
   final User? current_user;
@@ -18,11 +21,120 @@ class RouteSahaBul extends StatefulWidget {
 
 class _RouteSahaBulState extends State<RouteSahaBul> {
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late DateTime _dateTime = DateTime.now();
+  get firstNameEditingController => null;
+  String il = 'İl Seçiniz';
+  String ilce = 'İlçe Seçiniz';
+  bool _yuklemeTamamlandiMi = false;
+  String _secilenIl = "İl Secin";
+  String _secilenIlce = "İlce Seçin";
+  String? position;
+  Dropdownlist sehirler = new Dropdownlist();
+  String? number;
+  List<dynamic> _illerListesi = [];
+  List<String> _ilIsimleriListesi = [];
+  List<String> _ilceIsimleriListesi = [];
 
+  int _secilenIlIndexi = 0;
+  int _secilenIlceIndexi = 0;
+  bool _ilSecilmisMi = false;
+  bool _ilceSecilmisMi = false;
   @override
   Widget build(BuildContext context) {
     print(widget.current_user!.uid);
     CollectionReference sahaRef = firestore.collection('sahalar');
+    Future<void> _illeriGetir() async {
+      ilce = "";
+      if (sehirler.ilceler.isEmpty) {
+        sehirler.fillSehirs().then((value) => sehirler.fillIlces());
+      }
+      setState(() {
+        _yuklemeTamamlandiMi = true;
+      });
+    }
+
+    void _secilenIlinIlceleriniGetir(String _secilenIl) {
+      print(_secilenIl);
+      _ilceIsimleriListesi = [];
+      String? key;
+      for (var item in sehirler.illerListesi) {
+        if (item.sehir_title == _secilenIl) key = item.sehir_key;
+      }
+      if (key != null) {
+        sehirler.getIlces(key);
+        for (var item in sehirler.namesIlce) {
+          _ilceIsimleriListesi.add(item.ilce_title);
+        }
+      }
+      print(_ilceIsimleriListesi.length);
+      _ilSecilmisMi = true;
+    }
+
+    /// Il nesnelerinden sadece il_adi değişkenlerini ayrı bir listede toplama
+    void _ilIsimleriniGetir() {
+      _ilIsimleriListesi = [];
+      print(sehirler.namesIl.length);
+      sehirler.namesIl.forEach((element) {
+        _ilIsimleriListesi.add(element.sehir_title);
+      });
+      _yuklemeTamamlandiMi = true;
+    }
+
+    Future<void> _ilSecmeSayfasinaGit() async {
+      _ilIsimleriniGetir();
+      print(_yuklemeTamamlandiMi);
+      if (_yuklemeTamamlandiMi) {
+        if (!_ilIsimleriListesi.isEmpty) {
+          _secilenIlIndexi = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) =>
+                  IlSecimiSayfasi(ilIsimleri: _ilIsimleriListesi),
+            ),
+          );
+          _secilenIlceIndexi = 0;
+          _ilSecilmisMi = true;
+          if (_secilenIlIndexi != null) {
+            _secilenIl = _ilIsimleriListesi[_secilenIlIndexi];
+            _secilenIlinIlceleriniGetir(_secilenIl);
+            setState(() {});
+          } else {
+            _secilenIlIndexi = 0;
+          }
+        }
+      }
+    }
+
+    void updateInformation() {
+      final FirebaseFirestore firestore = FirebaseFirestore.instance;
+      CollectionReference userRef = firestore.collection('users');
+      userRef
+          .doc(FirebaseAuth.instance.currentUser!.uid
+              .toString()) // <-- Doc ID where data should be updated.
+          .update({
+        'town': _secilenIlce,
+        'city': _secilenIl,
+        'docId': FirebaseAuth.instance.currentUser!.uid.toString()
+      });
+    }
+
+    Future<void> _ilceSecmeSayfasinaGit() async {
+      if (_ilSecilmisMi) {
+        _secilenIl = _ilIsimleriListesi[_secilenIlIndexi];
+        _secilenIlinIlceleriniGetir(_secilenIl);
+        _secilenIlceIndexi = await Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                IlceSecmeSayfasi(ilceIsimleri: _ilceIsimleriListesi),
+          ),
+        );
+        _ilceSecilmisMi = true;
+        _secilenIlce = _ilceIsimleriListesi[_secilenIlceIndexi];
+        setState(() {});
+      }
+    }
+
     return Scaffold(
       floatingActionButton:
           true ? buildFloating(context, widget.current_user) : null,
@@ -37,6 +149,131 @@ class _RouteSahaBulState extends State<RouteSahaBul> {
         SafeArea(
             child: Column(
           children: [
+            /*InkWell(
+                onTap: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          content: Stack(
+                            overflow: Overflow.visible,
+                            children: <Widget>[
+                              Positioned(
+                                right: -40.0,
+                                top: -40.0,
+                                child: InkResponse(
+                                  onTap: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: CircleAvatar(
+                                    child: Icon(Icons.close),
+                                    backgroundColor: Colors.red,
+                                  ),
+                                ),
+                              ),
+                              Form(
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    InkWell(
+                                      onTap: () {
+                                        _illeriGetir().then((value) => {
+                                              _ilSecmeSayfasinaGit().then(
+                                                  (value) => il = _secilenIl)
+                                            });
+                                      },
+                                      child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 15, 20, 15),
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              flex: 1,
+                                              child: Icon(
+                                                Icons.location_on,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 5,
+                                              child: Text(
+                                                il,
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 20.0),
+                                              ),
+                                            ),
+                                            Icon(
+                                              Icons.keyboard_arrow_down,
+                                              color: Colors.black,
+                                            ),
+                                          ],
+                                        ),
+                                        decoration: BoxDecoration(
+                                            border: Border(
+                                                bottom: BorderSide(
+                                                    color: Colors.black,
+                                                    width: 2))),
+                                      ),
+                                    ),
+                                    InkWell(
+                                      onTap: () {
+                                        _illeriGetir()
+                                            .then((value) =>
+                                                _ilceSecmeSayfasinaGit())
+                                            .then(
+                                                (value) => ilce = _secilenIlce);
+                                      },
+                                      child: Container(
+                                        padding:
+                                            EdgeInsets.fromLTRB(0, 15, 20, 15),
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              flex: 1,
+                                              child: Icon(
+                                                Icons.location_on,
+                                                color: Colors.black,
+                                              ),
+                                            ),
+                                            Expanded(
+                                              flex: 5,
+                                              child: Text(
+                                                ilce,
+                                                style: TextStyle(
+                                                    color: Colors.black,
+                                                    fontSize: 20.0),
+                                              ),
+                                            ),
+                                            Icon(
+                                              Icons.keyboard_arrow_down,
+                                              color: Colors.black,
+                                            ),
+                                          ],
+                                        ),
+                                        decoration: BoxDecoration(
+                                            border: Border(
+                                                bottom: BorderSide(
+                                                    color: Colors.black,
+                                                    width: 2))),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: RaisedButton(
+                                        child: Text("Filtrele"),
+                                        onPressed: () async {},
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      });
+                },
+                child: Icon(Icons.filter_list, size: 35, color: Colors.white)),*/
             StreamBuilder<QuerySnapshot>(
               stream: sahaRef.snapshots(),
               builder: (BuildContext context, AsyncSnapshot asyncSnapshot) {
